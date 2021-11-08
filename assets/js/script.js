3c48e23ffd8c8e482f88bc330ec3403b2868ed94
NicholasCH <nicholasch96@gmail.com>
2021-11-07 18:42:38 -0600

    added html elements and in progress designing js

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// var sec = 120;
+// setInterval(countdownTimer, 1000);
+
+// function countdownTimer () {
+//     document.getElementById('countdownTimer').innerHTML = sec +" seconds remaining"
+//     sec--;
+//     if (sec== 0){
+//         clearInterval(time);
+//         timeUpScreen();
+//     }
+// }
+let questions = [
+  {
+    numb: 1,
+    question: "What is a mark-up language?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+ 
+  {
+    numb: 5,
+    question: "n?",
+    answer: "o",
+    options: [
+      "c",
+      "o",
+      "k",
+      "e"
+    ]//pepsi
+  },
+  // o will be correct answer
+];
+
